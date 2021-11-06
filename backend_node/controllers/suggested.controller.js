c09e1cf047ef8f161df201e1a5fb09bd165e1ef6
samiulalimsaad <saadnbiuraj@gmail.com>
2021-11-06 12:25:27 +0600

    fixed some node backend issue

--- backend_node/controllers/suggested.controller.js
+++ backend_node/controllers/suggested.controller.js
@@ -5,7 +5,19 @@ exports.getSuggestedUser = async (req, res) => {
         .find()
         .select("_id")
         .select("name profilePic following");
-    const suggestedUser = users.filter((v) => v._id !== req.params.id);
+    const suggestedUser = [
+        ...new Set(
+            users
+                .filter((v) => v._id !== req.params.id)
+                .map(
+                    (su) =>
+                        users.map((us) =>
+                            us.following.map((u) => u._id !== su._id && su)
+                        )[0]
+                )[0]
+        ),
+    ];
+
     return res.json({
         suggestedUser,
         success: true,
--- backend_node/test.js
+++ backend_node/test.js
@@ -1,95 +1,69 @@
-const moment = require("moment");
-
-// // const getExpired = (dat) => {
-// //     const date = [];
-// //     const time = [];
-// //     let d = "";
-// //     if (dat) d = new Date(dat);
-// //     else d = new Date();
-// //     time.push(d.getHours());
-// //     time.push(d.getMinutes());
-// //     time.push(d.getSeconds());
-// //     date.push(d.getDate() + 1);
-// //     date.push(d.getMonth() + 1);
-// //     date.push(d.getYear() + 1);
-// //     return [time.join("."), date.join("/")].join(" ");
-// // };
-// // console.log(getExpired())
-
-// // const a = "11.37.46 25/10/122"
-// // const b = a.split(" ")
-// // var startTime = b[0];
-// // var newTime = moment(startTime, "HH.MM.SS");
-// // var time = newTime.format("HH-MM-SS");
-// // console.log(time);
-// // var startdate = b[1];
-// // var new_date = moment(startdate, "DD.MM.YYYY");
-// // var thing = new_date.format("DD/MM/YYYY");
-// // console.log(thing);
-// // console.log(b)
-// let d = "October 23rd 2021, 12:16:18 pm";
-// // let a = moment().subtract(1, "days").format("MMMM Do YYYY, h:mm:ss a");
-
-// // console.log(moment(a, "MMMM Do YYYY, h:mm:ss a").startOf("day").fromNow());
-// console.log(
-//     moment(d, "MMMM Do YYYY, h:mm:ss a").endOf("day").fromNow().includes("in")
-// );
-
-const allStory = [
+const suggestedUser = [
     {
-        _id: "bbbbbbbbbbbbbb",
-        story: [
-            {
-                image: "zzz",
-                expired: "October 25th 2021, 11:56:20 am",
-                _id: "6174f584ccaa556f4272f83c",
-            },
-            {
-                image: "zzz",
-                expired: "October 25th 2021, 11:56:42 am",
-                _id: "6174f59accaa556f4272f847",
-            },
+        _id: "Qv7LUMKRbDYROf2HAyAZuuDgqTs2",
+        name: {
+            firstName: "bbb",
+            lastName: "bbb",
+            nickName: "",
+            fullName: "bbb bbb",
+            _id: "61767fca97b9dde2462a404c",
+        },
+        following: [],
+        follower: [
+            "qRCFrf1pXzYUunDwiAe33H1sxi52",
+            "qRCFrf1pXzYUunDwiAe33H1sxi52",
+            "qRCFrf1pXzYUunDwiAe33H1sxi52",
         ],
     },
     {
-        _id: "aaa",
-        story: [
-            {
-                image: "zzz",
-                expired: "October 25th 2021, 12:15:24 pm",
-                _id: "6174f9fc3a327ed28aceb035",
-            },
-            {
-                image: "zzz",
-                expired: "October 25th 2021, 12:15:51 pm",
-                _id: "6174fa173a327ed28aceb051",
-            },
-            {
-                image: "aaa",
-                expired: "October 23rd 2021, 12:16:18 pm",
-                _id: "6174fa32179af13438be249b",
-            },
-        ],
+        _id: "zYhkPrVNQ7RS5TYglwyLUL4MHoy1",
+        name: {
+            firstName: "ccc",
+            lastName: "ccc",
+            nickName: "",
+            fullName: "ccc ccc",
+            _id: "6176872429895578b152ab0e",
+        },
+        following: [],
+        follower: ["qRCFrf1pXzYUunDwiAe33H1sxi52"],
+    },
+    {
+        _id: "ahGcG7YHn7ZN049SAPLT74nUHV62",
+        name: {
+            firstName: "ddd",
+            lastName: "ddd",
+            nickName: "",
+            fullName: "ddd ddd",
+            _id: "6176875c29895578b152ab2a",
+        },
+        following: [],
+        follower: ["qRCFrf1pXzYUunDwiAe33H1sxi52"],
     },
 ];
 
-const story = [];
+const user = {
+    _id: "qRCFrf1pXzYUunDwiAe33H1sxi52",
+    email: "aaa@a.com",
+    active: true,
+    name: {
+        firstName: "aaa",
+        lastName: "aaa",
+        nickName: "",
+        fullName: "aaa aaa",
+        _id: "61763bad03c1fa25f30a61d5",
+    },
+    following: [
+        {
+            _id: "ahGcG7YHn7ZN049SAPLT74nUHV62",
+        },
+        {
+            _id: "zYhkPrVNQ7RS5TYglwyLUL4MHoy1",
+        },
+    ],
+};
 
-allStory.filter((v) =>
-    v.story.filter((s) => {
-        if (
-            moment(s.expired, "MMMM Do YYYY, h:mm:ss a")
-                .endOf("day")
-                .fromNow()
-                .includes("in")
-        ) {
-            console.log("object");
-            story.push({ id: v._id, ...s });
-        }
-    })
-);
+const a = [...new Set(suggestedUser.map(su=>user.following.map(u=>u._id!==su._id && su._id))[0])]
 
-
-// console.log(JSON.stringify(story, null, 4));
-console.log(JSON.stringify(story, null, 4));
-console.log(story.length);
+console.log(a)
+console.log(suggestedUser.length)
+console.log(a.length)
