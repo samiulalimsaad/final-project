b075e5eab71cf07807b020666456712aa3768733
samiulalimsaad <saadnbiuraj@gmail.com>
2022-01-19 15:40:41 +0600

    completed - 2

--- lite-media/components/progress/ProfileLoading.tsx
+++ lite-media/components/progress/ProfileLoading.tsx
@@ -3,7 +3,7 @@ import PostSkeleton from "./PostSkeleton";
 
 const ProfileLoading = () => {
     return (
-        <div className="pb-24 h-full overflow-y-scroll animate-puls">
+        <div className="pb-24 h-full overflow-y-scroll animate-pulse">
             <div className="relative mb-16">
                 <div className="bg-gray-300 h-44 w-full" />
                 <div className="absolute z-40 -bottom-12 px-2 w-full flex justify-between">
@@ -17,19 +17,17 @@ const ProfileLoading = () => {
                 </div>
             </div>
             <div className="flex items-center justify-between px-2 mb-3">
-                <>
-                    <div>
-                        <h1 className="flex space-x-2 font-semibold text-3xl">
-                            <span className="px-8 py-4 bg-gray-300 rounded-full" />
-                            <span className="px-8 py-4 bg-gray-300 rounded-full" />
-                        </h1>
-                        <h3 className="text-xs text-gray-600" />
-                    </div>
-                    <div className="flex flex-col justify-center pr-2">
-                        <span className="px-8 py-2 bg-gray-300 rounded-full" />
-                        <span className="px-8 py-2 mt-1 bg-gray-300 rounded-full" />
-                    </div>
-                </>
+                <div>
+                    <h1 className="flex space-x-2 font-semibold text-3xl">
+                        <span className="px-8 py-4 bg-gray-300 rounded-full" />
+                        <span className="px-8 py-4 bg-gray-300 rounded-full" />
+                    </h1>
+                    <h3 className="px-8 py-4 bg-gray-300 rounded-full" />
+                </div>
+                <div className="flex flex-col justify-center pr-2">
+                    <span className="px-8 py-2 bg-gray-300 rounded-full" />
+                    <span className="px-8 py-2 mt-1 bg-gray-300 rounded-full" />
+                </div>
             </div>
             <hr className="h-1 bg-gray-100" />
             <div className="mb-4">
